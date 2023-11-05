39b3a3deffd048c083e1e41e87ec8229a398663c
Mozzammel Hossen <53122797+abirpuc@users.noreply.github.com>
2023-11-05 13:00:33 +0600

    change section heading text style

--- src/components/Heading/SectionHeading.jsx
+++ src/components/Heading/SectionHeading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function SectionHeading({name, title, text}) {
     return (
-        <div className='text-center w-1/2 mx-auto'>
+        <div className='text-center w-full md:w-1/2 mx-auto'>
             <h5 className='text-cyan-500 text-2xl'>{name}</h5>
             <h1 className='text-xl md:text-4xl text-white font-bold my-2'>{title}</h1>
             <p>{text}</p>
